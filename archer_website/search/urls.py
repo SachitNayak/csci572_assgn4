319e15d6736c884b64efa49e1dc4a363077b0a45
SachitNayak <sachitmn@usc.edu>
2020-03-31 21:51:25 -0700

    adds a go back to search page link on results page.

--- archer_website/search/urls.py
+++ archer_website/search/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'search'
 urlpatterns = [
     path('', views.index, name='search_index'),
     path('results/', views.process, name='process_query'),
